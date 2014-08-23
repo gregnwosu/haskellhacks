0b3ce9b9d6318eca77096451718017a16afcf69d
Greg Nwosu <greg.nwosu@gmail.com>
2014-08-23 01:53:25 +0100

    codata exercise complete

--- codata/Main.hs
+++ codata/Main.hs
@@ -1,33 +1,51 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+module CoData where
+import           Control.Arrow
+
 data Lit  =
   StrLit String
   | IntLit Int
   | Ident String
     deriving (Show, Eq)
-data Expr
-  = Index Expr Expr
-  | Call Expr [Expr]
-  | Unary String Expr
-  | Binary Expr String Expr
-  | Paren Expr
+data Expr a
+  = Index a a
+  | Call a [a]
+  | Unary String a
+  | Binary a String a
+  | Paren a
   | Literal Lit
-    deriving (Show, Eq)
-
-data Stmt
- = Break
-   | Continue
-   | Empty
-   | IfElse Expr [Stmt] [Stmt]
-   | Return (Maybe Expr)
-   | While Expr [Stmt]
-   | Expression Expr
-     deriving (Show , Eq)
+    deriving (Show, Eq, Functor)
 
 
-flatten :: Expr -> Expr
-flatten (Literal i) = Literal i
+data Term f = In (f (Term f))
+
+out :: Term f -> f (Term f)
+out (In t) = t
 
 
-applyExpr :: (Expr -> Expr) -> Expr -> Expr
+
+topDown, bottomUp :: Functor a => (Term a -> Term a) -> Term a -> Term a
+bottomUp fn =
+  out                    -- 1) unpack
+  >>> fmap (bottomUp fn) -- 2) recurse
+  >>> In                 -- 3) repack
+  >>> fn                 -- 4) apply
+
+topDown f  =
+  In                    -- 1) pack
+  <<< fmap (topDown f)  -- 2) recurse
+  <<< out               -- 3) unpack
+  <<< f                 -- 4) apply
+
+flattenTerm :: Term Expr -> Term Expr
+flattenTerm (In (Paren e)) = e  -- remove all Parens
+flattenTerm other = other       -- do nothing otherwise
+
+flatten :: Term Expr -> Term Expr
+flatten = bottomUp flattenTerm
+
+applyExpr :: (a -> b) -> Expr a -> Expr b
 -- base case: applyExpr is the identity function on constants
 applyExpr f (Literal i) = Literal i
 
